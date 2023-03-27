0a8ff194f8c233b1613559c99d72bdc5eb65a997
ausias <ausias@hINFO6-8.xiam.cat>
2023-03-27 12:29:42 +0200

    pol

--- Uf4-Practica9/src/uf4/practica9/Uf4Practica9.java
+++ Uf4-Practica9/src/uf4/practica9/Uf4Practica9.java
@@ -1,9 +1,28 @@
 package uf4.practica9;
 
+import java.util.*;
+
 public class Uf4Practica9 {
 
+    static Scanner scan = new Scanner(System.in);
+
     public static void menu() {
-        
+        int opc;
+        System.out.println(" ");
+        System.out.println("----- VEHICLES -----");
+        System.out.println("1. Afegir Vehicle: ");
+        System.out.println("2. Afegir Helicopter: ");
+        System.out.println("3. Afegir Veler: ");
+        System.out.println("4. Afegir Cotxe: ");
+        System.out.println("5. Afegir Cotxe Electric: ");
+        System.out.println("6. Mostrar Vehicle: ");
+        System.out.println("7. Mostrar Helicopter: ");
+        System.out.println("8. Mostrar Veler: ");
+        System.out.println("9. Mostrar Cotxe: ");
+        System.out.println("10. Mostrar Cotxe Electric: ");
+        System.out.println("0. Sortir: ");
+        System.out.println("Elegeix una opció: ");
+        opc = scan.nextInt();
     }
 
     public static void main(String[] args) {
